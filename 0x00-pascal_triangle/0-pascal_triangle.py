a5bf475dcf1a514640441d7a850a35e7834cd1c1
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-05 05:55:51 +0100

    :broom: Remove type annotations

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 '''A module for working with Pascal's triangle.
 '''
-from typing import List
 
 
-def pascal_triangle(n: int) -> List[List[int]]:
+def pascal_triangle(n):
     '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.
     '''
