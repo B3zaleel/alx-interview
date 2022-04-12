823aa5eda74416177dff60332866e32ae9c7bd16
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-12 06:43:23 +0100

    :adhesive_bandage: Fix issue of accessing key of an unavailable box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,11 +8,14 @@ def canUnlockAll(boxes):
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
     '''
+    n = len(boxes)
     seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference(set([0]))
     while len(unseen_boxes) > 0:
         boxIdx = unseen_boxes.pop()
-        if boxIdx and boxIdx not in seen_boxes:
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
             unseen_boxes = unseen_boxes.union(boxes[boxIdx])
             seen_boxes.add(boxIdx)
-    return len(boxes) == len(seen_boxes)
+    return n == len(seen_boxes)
