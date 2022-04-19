29e7f882b3638e1fa52e3a67f60f5717ba57f826
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-19 06:53:41 +0100

    :wrench: Fix issues with minimum operations algorithm

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,30 +7,29 @@ def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
     '''
-    if n <= 0:
+    if not isinstance(n, int):
         return 0
-    prime_factors = (1, 2, 3)
-    build_ops_count = (2, 3, 4)
-    prime_freq = [1, 0, 0]
-    i = len(prime_factors) - 1
-    rem = n
     ops_count = 0
     clipboard = 0
-    while rem > 0:
-        if rem % prime_factors[i] == 0:
-            if rem == rem / prime_factors[i]:
-                break
-            prime_freq[i] += 1
-            rem -= prime_factors[i]
-            if clipboard > prime_factors[i]:
-                return 0
-            if clipboard == prime_factors[i]:
-                ops_count += 1
-                continue
-            ops_count += build_ops_count[i]
-            clipboard = prime_factors[i]
-        elif i >= 0:
-            i -= 1
-        else:
-            return 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
     return ops_count
