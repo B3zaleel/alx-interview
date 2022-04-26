030a714808ddf10417faa57ab8c4b4c6f085c299
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-26 20:11:12 +0100

    :wrench: Refactor script

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,39 +4,33 @@
 import re
 
 
-status_codes_stats = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-'''Stats for the supported status codes.
-'''
-total_file_size = 0
-'''The cummulative sum of the file sizes in each HTTP request log.
-'''
-fp = (
-    r'\s*(?P<ip>\S+)\s*',
-    r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-    r'\s*"(?P<request>[^"]*)"\s*',
-    r'\s*(?P<status_code>\S+)',
-    r'\s*(?P<file_size>\d+)'
-)
-'''The pattern for each field in the log.
-'''
-log_fmt = f'{fp[0]}\\-{fp[1]}{fp[2]}{fp[3]}{fp[4]}\\s*'
-'''The regex pattern for the log.
-'''
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = f'{fp[0]}\\-{fp[1]}{fp[2]}{fp[3]}{fp[4]}\\s*'
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
 
-def print_statistics():
+def print_statistics(total_file_size, status_codes_stats):
     '''Prints the accumulated statistics of the HTTP request log.
     '''
-    global total_file_size, status_codes_stats
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
@@ -44,35 +38,50 @@ def print_statistics():
             print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
-def get_metrics(line):
-    '''Retrieves the metrics from a given HTTP request log.
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
 
     Args:
         line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
     '''
-    global total_file_size, log_fmt, status_codes_stats
-    resp_match = re.fullmatch(log_fmt, line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        total_file_size += file_size
-        if status_code in status_codes_stats.keys():
-            status_codes_stats[status_code] += 1
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
 
 def run():
     '''Starts the log parser.
     '''
     line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
         while True:
             line = input()
-            get_metrics(line)
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
             line_num += 1
             if line_num % 10 == 0:
-                print_statistics()
+                print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
-        print_statistics()
+        print_statistics(total_file_size, status_codes_stats)
 
 
 if __name__ == '__main__':
