a15278aa2ba47f3336ef89a39b1d4500d5253880
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 08:45:39 +0100

    :wrench: Remove support for 3 and 4 byte UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,8 +7,19 @@ def validUTF8(data):
     """Checks if a list of integers are valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
+    byte_parts = []
+    for c in data:
+        if c >= 0x00 and c <= 0x7f:
+            byte_parts.append(c)
+        elif c >= 0x80 and c <= 0x7ff:
+            byte_parts.append((c >> 6) | 0b11000000)
+            byte_parts.append(c - (1 << 6))
+        else:
+            return False
     try:
-        _ = bytes(data).decode("utf-8", "strict")
-    except Exception:
+        byte_str = bytes(byte_parts).decode("utf-8", "strict")
+        print(data, ":", byte_str)
+    except Exception as ex:
+        print(ex)
         return False
     return True
