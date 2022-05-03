36bbf1de2bb98a3283699a607e2568cadf012744
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 06:54:02 +0100

    :wrench: Change UTF-8 validation algorithm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,21 +7,8 @@ def validUTF8(data):
     """Checks if a list of integers are valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    valid_flags = []
-
-    def bin_len(c_byte):
-        return len(bin(c_byte)[2:])
-    for c in data:
-        if type(c) != int:
-            return False
-        if c >= 0x00 and c <= 0x7f:
-            valid_flags = [bin_len(c) <= 7]
-        if c >= 0x80 and c <= 0x7ff:
-            valid_flags = [bin_len(c) <= 11]
-        if c >= 0x800 and c <= 0xffff:
-            valid_flags = [bin_len(c) <= 16]
-        if c >= 0x10000 and c <= 0x10FFFF:
-            valid_flags = [bin_len(c) <= 21]
-        if not all(valid_flags) or len(valid_flags) == 0:
-            return False
+    try:
+        _ = bytes(data).decode("utf-8", "strict")
+    except Exception:
+        return False
     return True
