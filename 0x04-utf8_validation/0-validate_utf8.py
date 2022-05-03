08a3b1530d8256ea49cb8eacfc79ab4d41e8ec05
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 11:51:02 +0100

    :wrench: Change body slice range

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,7 +23,7 @@ def validUTF8(data):
             if n - i >= span:
                 next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: span],
+                    data[i + 1: i + span],
                 ))
                 if not all(next_body):
                     return False
@@ -36,7 +36,7 @@ def validUTF8(data):
             if n - i >= span:
                 next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: span],
+                    data[i + 1: i + span],
                 ))
                 if not all(next_body):
                     return False
@@ -49,7 +49,7 @@ def validUTF8(data):
             if n - i >= span:
                 next_body = list(map(
                     lambda x: x & 0b11000000 == 0b10000000,
-                    data[i + 1: span],
+                    data[i + 1: i + span],
                 ))
                 if not all(next_body):
                     return False
