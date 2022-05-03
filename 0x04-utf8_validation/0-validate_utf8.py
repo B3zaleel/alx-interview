42be35aa7840dd5dc4bde3265a3ae4cf5be9d395
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 11:24:16 +0100

    :wrench: Change UTF-8 validation algorithm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,7 +13,7 @@ def validUTF8(data):
         if skip > 0:
             skip -= 1
             continue
-        if data[i] < 0 or type(data[i]) != int:
+        if type(data[i]) != int or data[i] < 0:
             return False
         elif data[i] <= 0x7f:
             skip = 0
