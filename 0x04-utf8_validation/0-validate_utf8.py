b50e7dbc28105682c5e6d3d8749ad3045633ce64
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 09:01:56 +0100

    :wrench: Change UTF-8 validation algorithm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,17 +7,10 @@ def validUTF8(data):
     """Checks if a list of integers are valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    byte_parts = []
-    for c in data:
-        if c >= 0x00 and c <= 0x7f:
-            byte_parts.append(c)
-        elif c >= 0x80 and c <= 0x7ff:
-            byte_parts.append((c >> 6) | 0b11000000)
-            byte_parts.append(c - (1 << 6))
-        else:
-            return False
+    if data is None:
+        return False
     try:
-        _ = bytes(byte_parts).decode("utf-8", "strict")
+        _ = bytes(data).decode("utf-8", "strict")
     except Exception:
         return False
     return True
