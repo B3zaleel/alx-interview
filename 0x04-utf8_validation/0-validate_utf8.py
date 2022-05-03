cd2bb08f3a8af99a7debe553b8b79d0ddf4cdf53
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 06:28:15 +0100

    :wrench: Change UTF-8 validation algorithm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """UTF-8 validation module.
 """
-import struct
 
 
 def validUTF8(data):
@@ -9,33 +8,19 @@ def validUTF8(data):
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
     valid_flags = []
+    def bin_len(c_byte):
+        return len(bin(c_byte)[2:])
     for c in data:
         if type(c) != int:
             return False
         if c >= 0x00 and c <= 0x7f:
-            c_bytes = struct.pack(">B", c)
-            valid_flags = [c_bytes[0] <= 0b01111111]
+            valid_flags = [bin_len(c) <= 7]
         if c >= 0x80 and c <= 0x7ff:
-            c_bytes = struct.pack(">H", c)
-            valid_flags = [
-                c_bytes[0] <= 0b10111111,
-                c_bytes[1] <= 0b11011111
-            ]
+            valid_flags = [bin_len(c) <= 11]
         if c >= 0x800 and c <= 0xffff:
-            c_bytes = struct.pack(">I", c)
-            valid_flags = [
-                (c_bytes[1] <= 0b10111111),
-                (c_bytes[2] <= 0b10111111),
-                (c_bytes[3] <= 0b11101111)
-            ]
+            valid_flags = [bin_len(c) <= 16]
         if c >= 0x10000 and c <= 0x10FFFF:
-            c_bytes = struct.pack(">I", c)
-            valid_flags = [
-                (c_bytes[0] <= 0b10111111),
-                (c_bytes[1] <= 0b10111111),
-                (c_bytes[2] <= 0b10111111),
-                (c_bytes[3] <= 0b11110111)
-            ]
+            valid_flags = [bin_len(c) <= 21]
         if not all(valid_flags) or len(valid_flags) == 0:
             return False
     return True
