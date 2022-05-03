e59bddc4efbac8851cb0aa7f0aac91e80f737597
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 06:36:35 +0100

    :wrench: Remove support for multi-byte UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,16 +12,10 @@ def validUTF8(data):
     def bin_len(c_byte):
         return len(bin(c_byte)[2:])
     for c in data:
-        if type(c) != int:
+        if type(c) != int or c < 0:
             return False
         if c >= 0x00 and c <= 0x7f:
             valid_flags = [bin_len(c) <= 7]
-        if c >= 0x80 and c <= 0x7ff:
-            valid_flags = [bin_len(c) <= 11]
-        if c >= 0x800 and c <= 0xffff:
-            valid_flags = [bin_len(c) <= 16]
-        if c >= 0x10000 and c <= 0x10FFFF:
-            valid_flags = [bin_len(c) <= 21]
         if not all(valid_flags) or len(valid_flags) == 0:
             return False
     return True
