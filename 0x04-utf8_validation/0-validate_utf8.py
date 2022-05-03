f3c8d9acb6e25212d7a8e73772207d4b2362b8ec
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 06:06:43 +0100

    :wrench: Fix SyntaxError issues with Python 3.4

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,27 +14,27 @@ def validUTF8(data):
             return False
         if c >= 0x00 and c <= 0x7f:
             c_bytes = struct.pack(">B", c)
-            valid_flags = [c_bytes[0] <= 0b0111_1111]
+            valid_flags = [c_bytes[0] <= 0b01111111]
         if c >= 0x80 and c <= 0x7ff:
             c_bytes = struct.pack(">H", c)
             valid_flags = [
-                c_bytes[0] <= 0b1011_1111,
-                c_bytes[1] <= 0b1101_1111
+                c_bytes[0] <= 0b10111111,
+                c_bytes[1] <= 0b11011111
             ]
         if c >= 0x800 and c <= 0xffff:
             c_bytes = struct.pack(">I", c)
             valid_flags = [
-                (c_bytes[1] <= 0b1011_1111),
-                (c_bytes[2] <= 0b1011_1111),
-                (c_bytes[3] <= 0b1110_1111)
+                (c_bytes[1] <= 0b10111111),
+                (c_bytes[2] <= 0b10111111),
+                (c_bytes[3] <= 0b11101111)
             ]
         if c >= 0x10000 and c <= 0x10FFFF:
             c_bytes = struct.pack(">I", c)
             valid_flags = [
-                (c_bytes[0] <= 0b1011_1111),
-                (c_bytes[1] <= 0b1011_1111),
-                (c_bytes[2] <= 0b1011_1111),
-                (c_bytes[3] <= 0b1111_0111)
+                (c_bytes[0] <= 0b10111111),
+                (c_bytes[1] <= 0b10111111),
+                (c_bytes[2] <= 0b10111111),
+                (c_bytes[3] <= 0b11110111)
             ]
         if not all(valid_flags) or len(valid_flags) == 0:
             return False
