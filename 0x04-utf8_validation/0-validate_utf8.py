e55ea112b0e85f8086e33d3b965e12b3a6c6f23e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 06:00:49 +0100

    :wrench: Change UTF-8 validation algorithm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 """UTF-8 validation module.
 """
+import struct
 
 
 def validUTF8(data):
     """Checks if a list of integers are valid UTF-8 codepoints.
-    See the Encoding section at <https://en.wikipedia.org/wiki/UTF-8>
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    if not isinstance(data, (list, tuple, set)):
-        return False
     valid_flags = []
     for c in data:
         if type(c) != int:
             return False
         if c >= 0x00 and c <= 0x7f:
-            valid_flags = [(c >> 7) & 0b1 == 0b0]
+            c_bytes = struct.pack(">B", c)
+            valid_flags = [c_bytes[0] <= 0b0111_1111]
         if c >= 0x80 and c <= 0x7ff:
+            c_bytes = struct.pack(">H", c)
             valid_flags = [
-                (c >> 6) & 0b10 == 0b10,
-                (c >> 13) & 0b110 == 0b110
+                c_bytes[0] <= 0b1011_1111,
+                c_bytes[1] <= 0b1101_1111
             ]
         if c >= 0x800 and c <= 0xffff:
+            c_bytes = struct.pack(">I", c)
             valid_flags = [
-                (c >> 6) & 0b10 == 0b10,
-                (c >> 14) & 0b10 == 0b10,
-                (c >> 20) & 0b1110 == 0b1110
+                (c_bytes[1] <= 0b1011_1111),
+                (c_bytes[2] <= 0b1011_1111),
+                (c_bytes[3] <= 0b1110_1111)
             ]
         if c >= 0x10000 and c <= 0x10FFFF:
+            c_bytes = struct.pack(">I", c)
             valid_flags = [
-                (c >> 6) & 0b10 == 0b10,
-                (c >> 14) & 0b10 == 0b10,
-                (c >> 22) & 0b10 == 0b10,
-                (c >> 27) & 0b11110 == 0b11110
+                (c_bytes[0] <= 0b1011_1111),
+                (c_bytes[1] <= 0b1011_1111),
+                (c_bytes[2] <= 0b1011_1111),
+                (c_bytes[3] <= 0b1111_0111)
             ]
         if not all(valid_flags) or len(valid_flags) == 0:
             return False
