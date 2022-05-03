386038b152eef838cc1736b7a58c6136016c5e6f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-03 10:23:50 +0100

    :wrench: Change UTF-8 validation algorithm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,10 +7,55 @@ def validUTF8(data):
     """Checks if a list of integers are valid UTF-8 codepoints.
     See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    if data is None:
+    if not isinstance(data, (list, tuple)):
         return False
-    try:
-        _ = bytes(data).decode("utf-8")
-    except Exception:
-        return False
-    return True
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
+        if data[i] < 0:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif (data[i] >> 3) & 0b11111 == 0b11110:
+            # 4-byte utf-8 character encoding
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: (x >> 6) | 0b10 == 0b10,
+                    data[i + 1: span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif (data[i] >> 4) & 0b1111 == 0b1110:
+            # 3-byte utf-8 character encoding
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: (x >> 6) | 0b10 == 0b10,
+                    data[i + 1: span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif (data[i] >> 5) & 0b111 == 0b110:
+            # 2-byte utf-8 character encoding
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: (x >> 6) | 0b10 == 0b10,
+                    data[i + 1: span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+    return skip == 0
