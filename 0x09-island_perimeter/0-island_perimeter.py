0b2da7fdc46dd3a81365ef33cfb4e3bcd19e3ad5
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-06 05:34:51 +0100

    :sparkles: Complete island perimeter challenge

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Island perimeter computing module.
+"""
+
+
+def island_perimeter(grid):
+    """Computes the perimeter of an island with no lakes.
+    """
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
