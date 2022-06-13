78cc172896eaad86fb3b444641bd989788f6988f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-13 09:09:10 +0100

    :sparkles: Complete prime game challenge

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Prime game module.
+"""
+
+
+def isWinner(x, nums):
+    """Determines the winner of a prime game session with `x` rounds.
+    """
+    players = ('Maria', 'Ben')
+    winners = []
+    nums_len = len(nums) if nums else 0
+    if x <= 0:
+        return None
+    for i in range(x):
+        n = nums[i % nums_len] if nums else 0
+        n_nums = list(range(1, n + 1, 1))
+        prime = 2
+        turns = 0
+        while True:
+            removal_ocurred = False
+            p_multiples = list(range(prime, n + 1, prime))
+            for p_multiple in p_multiples:
+                if p_multiple in n_nums:
+                    n_nums.remove(p_multiple)
+                    removal_ocurred = True
+            turns += 1
+            if removal_ocurred:
+                for val in n_nums:
+                    if val > prime:
+                        prime = val
+                        break
+            else:
+                break
+        winners.append(players[turns % 2])
+    marias_wins = winners.count(players[0])
+    bens_wins = winners.count(players[1])
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
--- 0x0A-primegame/README.md
+++ 0x0A-primegame/README.md
@@ -4,7 +4,7 @@ This project contains interview coding challenges.
 
 ## Tasks To Complete
 
-+ [ ] 0. **Prime Game**<br/>[0-prime_game.py](0-prime_game.py) contains the solution to the following challenge:
++ [x] 0. **Prime Game**<br/>[0-prime_game.py](0-prime_game.py) contains the solution to the following challenge:
   + **INFO:**
     + Maria and Ben are playing a game. Given a set of consecutive integers starting from `1` up to and including `n`, they take turns choosing a prime number from the set and removing that number and its multiples from the set. The player that cannot make a move loses the game.
     + They play `x` rounds of the game, where `n` may be different for each round. Assuming Maria always goes first and both players play optimally, determine who the winner of each game is.
