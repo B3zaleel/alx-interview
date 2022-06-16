89fee2bb4c117637d2a8d0721e00132aa81af1e6
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-16 21:29:15 +0100

    :alembic: Testing alternate solution

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,34 +6,26 @@
 def isWinner(x, nums):
     """Determines the winner of a prime game session with `x` rounds.
     """
-    players = ('Maria', 'Ben')
-    winners = []
-    nums_len = len(nums) if nums else 0
-    if nums_len == 0:
+    if not nums or x < 1:
         return None
-    for i in range(x):
-        n = nums[i] if i < nums_len else 0
-        n_nums = list(range(1, n + 1, 1))
-        prime = 2
-        turns = 0
-        while True:
-            removal_ocurred = False
-            p_multiples = list(range(prime, n + 1, prime))
-            for p_multiple in p_multiples:
-                if p_multiple in n_nums:
-                    n_nums.remove(p_multiple)
-                    removal_ocurred = True
-            turns += 1
-            if removal_ocurred:
-                for val in n_nums:
-                    if val > prime:
-                        prime = val
-                        break
-            else:
-                break
-        winners.append(players[turns % 2])
-    marias_wins = winners.count(players[0])
-    bens_wins = winners.count(players[1])
-    if marias_wins == bens_wins:
+    n = max(nums)
+    fltr = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not fltr[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            fltr[j] = False
+    fltr[0] = fltr[1] = False
+    c = 0
+    for i in range(len(fltr)):
+        if fltr[i]:
+            c += 1
+        fltr[i] = c
+    plyr1 = 0
+    for n in nums:
+        plyr1 += fltr[n] % 2 == 1
+    if plyr1 * 2 == len(nums):
         return None
-    return 'Maria' if marias_wins > bens_wins else 'Ben'
+    if plyr1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
