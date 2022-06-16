1991954ecf428193de31f5cb45b342c930009d1f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-16 22:24:38 +0100

    Update prime game winner detection algorithm

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,6 +3,14 @@
 """
 
 
+def is_prime(num):
+    num_sqrt = int(num ** 0.5 + 1)
+    for i in range(2, num_sqrt):
+        if num % i == 0:
+            return False
+    return True
+
+
 def isWinner(x, nums):
     """Determines the winner of a prime game session with `x` rounds.
     """
@@ -14,21 +22,23 @@ def isWinner(x, nums):
     for i in range(x):
         n = nums[i] if i < nums_len else 0
         n_nums = list(range(1, n + 1, 1))
-        prime = 2
-        turns = 0
+        prime, turns = 2, 0
         while True:
             removal_ocurred = False
-            p_multiples = list(range(prime, n + 1, prime))
-            for p_multiple in p_multiples:
-                if p_multiple in n_nums:
-                    n_nums.remove(p_multiple)
+            for multiple in range(prime, n + 1, prime):
+                if multiple in n_nums:
+                    n_nums.remove(multiple)
                     removal_ocurred = True
             turns += 1
             if removal_ocurred:
+                next_prime_chosen = False
                 for val in n_nums:
-                    if val > prime:
+                    if val > prime and is_prime(val):
                         prime = val
+                        next_prime_chosen = True
                         break
+                if not next_prime_chosen:
+                    break
             else:
                 break
         winners.append(players[turns % 2])
